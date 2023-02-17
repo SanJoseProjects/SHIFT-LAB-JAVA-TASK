35b763ffab772d531b4660bbb33cded79c936259
SanJoseProjects <ilyagorbachev7@gmail.com>
2023-02-17 11:20:21 +0700

    Initial commit

---
+++ src/main/java/com/example/shiftlabjavatask/ShiftLabJavaTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.shiftlabjavatask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftLabJavaTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiftLabJavaTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shiftlabjavatask/ShiftLabJavaTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shiftlabjavatask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftLabJavaTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
